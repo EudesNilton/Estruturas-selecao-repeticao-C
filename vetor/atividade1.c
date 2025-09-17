4d3e5ac6293d92b8a8238088f76bb5e3e54344f3
Eudes  Nilton <129069744+EudesNilton@users.noreply.github.com>
2025-09-17 20:54:10 -0300

    Atividade de vetor

---
+++ vetor/atividade1.c
@@ -0,0 +1,12 @@
+#include <stdio.io>
+
+int main()
+{
+    int vetor[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+    
+    for(int i = 0; i < 10;i++){
+        printf("Os numeros do vetor são: %d", vetor[i]);
+        printf("\n");
+    }
+    return 0;
+}
